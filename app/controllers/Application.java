c23e3f6c1569c5e631b0389ddde2d61ce500653e
Matthieu Maquevice <matth.maq@gmail.com>
2013-09-23 19:56:37 +0200

    remote neo4j

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,15 +4,19 @@ import java.util.List;
 
 import models.World;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import play.*;
+import play.Logger.ALogger;
 import play.mvc.*;
 import services.GalaxyService;
 import views.html.*;
 
 @org.springframework.stereotype.Controller
 public class Application extends Controller {
+
+	ALogger log = Logger.of(Application.class);
 
 	@Autowired
 	GalaxyService galaxyService;
@@ -26,6 +30,8 @@ public class Application extends Controller {
 		World first = worlds.get(0);
 		World last = worlds.get(worlds.size() - 1);
 		List<World> pathWorlds = galaxyService.getWorldPath(first, last);
+
+		log.info("toto");
 
 		return ok(index.render("Your new application is ready."));
 	}
